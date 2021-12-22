97de16ed6dfc8301cb3c7d0de578c073f08af99d
Robin de Witte <r.dewitte@student.fontys.nl>
2021-12-22 14:23:32 +0100

    poging zoveel servicename

--- src/app/helpers/restdata.ts
+++ src/app/helpers/restdata.ts
@@ -4,7 +4,7 @@ export class Restdata {
 
 //voor nu nog even localhost
 //  private url = 'http://localhost:5001/';
-private url = 'gateway/'
+private url = 'gateway.default.svc.cluster.local:5001/'
 
 
   public httpOptionsToken = { 
@@ -15,7 +15,7 @@ private url = 'gateway/'
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': 'Bearer ' +sessionStorage.getItem("token"),
-      'Access-Control-Allow-Origin': 'gateway/',
+      'Access-Control-Allow-Origin': 'gateway.default.svc.cluster.local:5001/',
       'Access-Control-Allow-Credentials': 'true'
     })
   };
